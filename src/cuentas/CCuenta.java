2c667d2591ba4bbf748cc043530418dd641a1a9f
andre <andre@192.168.1.6>
2022-02-16 19:30:48 +0100

    Tarea para ED04 primer commint

---
+++ src/cuentas/CCuenta.java
@@ -0,0 +1,89 @@
+package cuentas;
+/**
+ * 
+ * @author andre
+ *
+ */
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInter√s;
+
+    public CCuenta()
+    {
+    }
+/**
+ * 
+ * @param nom
+ * @param cue
+ * @param sal
+ * @param tipo
+ */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        setNombre(nom);
+        setCuenta(cue);
+        saldo=sal;
+    }
+/**
+ * 
+ * @return
+ */
+    public double estado()
+    {
+        return saldo;
+    }
+/**
+ * 
+ * @param cantidad
+ * @throws Exception
+ */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+/**
+ * 
+ * @param cantidad
+ * @throws Exception
+ */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getTipoInter√s() {
+		return tipoInter√s;
+	}
+
+	public void setTipoInter√s(double tipoInter√s) {
+		this.tipoInter√s = tipoInter√s;
+	}
+	
+	
+}
